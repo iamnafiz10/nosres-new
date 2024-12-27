7b135fd6007ecc227e8da018f7eda6d492fcd0dc
Roshan SaiHam Nafiz <roshansaihamnafiz@gmail.com>
2024-12-27 15:30:15 +0600

    Upload again

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    missingSuspenseWithCSRBailout: false,
+    /* config options here */
 };
+
+module.exports = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+}
 
 export default nextConfig;
