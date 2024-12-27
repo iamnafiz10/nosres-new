74df9f360a9d9f641ef1e980e33b144c957a88b8
Roshan SaiHam Nafiz <roshansaihamnafiz@gmail.com>
2024-12-27 16:30:15 +0600

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import {Suspense} from "react";
 import React from 'react';
 import Header from './(SITE ONE)/home/components/header/page'
 import Footer from './(SITE ONE)/home/components/footer/page'
@@ -8,8 +9,10 @@ function Page() {
     return (
         <>
             <BrowserRouter>
-                <Header/>
-                <Footer/>
+                <Suspense>
+                    <Header/>
+                    <Footer/>
+                </Suspense>
             </BrowserRouter>
         </>
     );
